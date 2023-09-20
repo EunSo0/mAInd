cc3e0b25d79c71eb21be1d0c3c7bfb34f52e1e41
EunSo0 <dmstn423@naver.com>
2023-09-20 11:08:34 +0900

    Feat: 헤더수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
-import Modal from "../components/LoginModal";
+// import Modal from "../components/LoginModal";
+
+const MenuList = [
+  { name: "상담하기", page: "/meeting" },
+  { name: "상담예약", page: "/initial_survey" },
+  { name: "마이페이지", page: "/mypage" },
+];
 
 export default function Header() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+  // const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const closeModal = () => {
-    setIsModalOpen(false);
+  // const closeModal = () => {
+  //   setIsModalOpen(false);
+  // };
+
+  const loginState = () => {
+    setIsLogin((prev) => !prev);
   };
 
   return (
@@ -20,28 +31,26 @@ export default function Header() {
               <LogoTxt>비대면 심리 상담 서비스</LogoTxt>
             </Link>
           </LogoWrapper>
-
-          {/* <NavLinks>
-          <NavLink>
-            <Link to="/meeting">상담하기</Link>
-          </NavLink>
-          <NavLink>
-            <Link to="/initial_survey">상담예약</Link>
-          </NavLink>
-          <NavLink>
-            <Link to="/result">상담기록</Link>
-          </NavLink>
-          <NavLink> */}
-
-          <Link to="/">
-            <Login onClick={() => setIsModalOpen(true)}>로그인/회원가입</Login>
-          </Link>
-
-          {/* </NavLink>
-        </NavLinks> */}
+          {isLogin && (
+            <LinkWrapper>
+              <NavLinks>
+                {MenuList.map((el) => (
+                  <NavLink key={el.page}>
+                    <Link to={el.page}>{el.name}</Link>
+                  </NavLink>
+                ))}
+              </NavLinks>
+              <Name>김상담님</Name>
+              <RoleBtn>상담자</RoleBtn>
+            </LinkWrapper>
+          )}
+          {!isLogin && (
+            // <Login onClick={() => setIsModalOpen(true)}>로그인/회원가입</Login>
+            <Login onClick={loginState}>로그인/회원가입</Login>
+          )}
         </Inner>
       </Wrapper>
-      {isModalOpen && <Modal isOpen={isModalOpen} onClose={closeModal} />}
+      {/* {isModalOpen && <Modal isOpen={isModalOpen} onClose={closeModal} />} */}
     </>
   );
 }
@@ -89,29 +98,51 @@ const LogoTxt = styled.div`
   color: #36f;
   text-align: center;
 `;
+const LinkWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+const NavLinks = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+`;
 
-// const NavLinks = styled.ul`
-//   display: flex;
-//   align-items: center;
-//   list-style: none;
-// `;
+const NavLink = styled.div`
+  margin: 0 20px;
+  font-size: 18px;
+  color: #666;
+  font-family: "Montserrat", sans-serif;
+  transition: color 0.4s;
 
-// const NavLink = styled.li`
-//   margin: 0 20px;
-//   font-size: 20px;
-//   color: #000;
-//   font-family: "Montserrat", sans-serif;
-//   transition: color 0.4s;
+  a {
+    text-decoration: none;
+    color: inherit;
 
-//   a {
-//     text-decoration: none;
-//     color: inherit;
+    &:hover {
+      color: #000;
+    }
+  }
+`;
 
-//     &:hover {
-//       color: #ff6b6b;
-//     }
-//   }
-// `;
+const Name = styled.div`
+  color: #000;
+  font-size: 18px;
+  font-weight: 700;
+  margin-right: 30px;
+`;
+const RoleBtn = styled.div`
+  display: flex;
+  padding: 8px 18px;
+  border-radius: 16px;
+  background: #14ae5c;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 24px;
+`;
 
 const Login = styled.div`
   background-color: #3366ff;
@@ -120,4 +151,5 @@ const Login = styled.div`
   padding: 8px 16px;
   margin-left: 50px;
   border-radius: 16px;
+  cursor: pointer;
 `;
